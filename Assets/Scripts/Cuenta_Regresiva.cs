b48a5be155d8462d2175b370e5e77cc9bf985604
Matias-Arisi-Guliasdad <49960010@est.ort.edu.ar>
2026-04-23 10:40:05 -0300

    Initial commit

---
+++ Assets/Scripts/Cuenta_Regresiva.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cuenta_Regresiva : MonoBehaviour
+{
+    public TMP_InputField input_IngreseNumero;
+    public TextMeshProUGUI txt_Resultado;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void CuentaRegresiva()
+    {
+        int numero = int.Parse(input_IngreseNumeros.text);
+        numero = numero - 1;
+        resultado_text.text = numero.ToString();
+    }
+}
